958b3ce8bb01769ef77c6d2c98dd20928b6e84b1
Ashwin <aswinjeganath@gmail.com>
2020-01-02 23:26:23 +0530

    add react router, set up routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 
-import Dashboard from './components/Dashboard'
+import IndexPage from './pages/'
+import Dashboard from './pages/Dashboard'
 import GlobalStyles from './components/base'
 
 const App = () => {
   return (
     <div className="App">
       <GlobalStyles />
-      <Dashboard />
+      <Route exact path="/" component={IndexPage} />
+      <Route exact path="/dashboard" component={Dashboard} />
     </div>
   )
 }
