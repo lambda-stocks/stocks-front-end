813e356cd5e740b13b00cf96e452e78f3281f2f0
johnathanhuggett <johnathan.huggett@amount.com>
2020-01-11 12:18:33 -0500

    added media example

--- src/theme/Media.js
+++ src/theme/Media.js
@@ -8,6 +8,11 @@ const sizes = {
   xsmall: null
 }
 
+/* ---------------------------------------------------------------------
+  Example Usage:
+    ${({theme: {breakPoints: { large }}}) => large`CSS_PROPERTIES_GO_HERE`}
+------------------------------------------------------------------------ */
+
 const Media = Object.keys(sizes).reduce((accumulator, label) => {
   accumulator[label] = (...args) => css`
     @media (max-width: ${sizes[label] / 16}em) {
